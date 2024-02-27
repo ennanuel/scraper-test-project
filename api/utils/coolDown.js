8dd1c76c1730183b239587843da8640b1b82eef9
Emmanuel Ezema <emmanuelezema6@gmail.com>
2024-02-27 07:52:25 +0100

    Refactored code

--- api/utils/coolDown.js
+++ api/utils/coolDown.js
@@ -1,16 +1,18 @@
 import { getLogJSON } from "./log.js";
 
-const COOL_DOWN_TIME_IN_MS = 10000;
+// This logic is esures that each call to Amazon's website is made 10 seconds at a time.
+
 const TEN_SECONDS_IN_MS = 10000;
 
-const waitForCoolDown = () => new Promise((resolve) => setTimeout(resolve, COOL_DOWN_TIME_IN_MS));
+const waitForCoolDown = (delayInMilliseconds) => new Promise((resolve) => setTimeout(resolve, delayInMilliseconds));
 
 function checkIfShouldWaitForCoolDown() {
     const log = getLogJSON();
-    const lastTimeLogWasCalled = log.lastCalled
+    const lastTimeLogWasCalled = log.lastCalled;
     const timePassed = Date.now() - lastTimeLogWasCalled;
     const shouldWaitForCoolDown = timePassed < TEN_SECONDS_IN_MS;
-    return shouldWaitForCoolDown;
+    const byHowLong = TEN_SECONDS_IN_MS - timePassed;
+    return { shouldWaitForCoolDown, byHowLong };
 }
 
 export {
